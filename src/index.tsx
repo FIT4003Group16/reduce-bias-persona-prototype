cc1af7e1f3d5a3260ed643f49ee141c304bf3b3f
chuntse <chuntsexj@gmail.com>
2021-08-17 17:25:23 +0800

    init prototype

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC, useState } from 'react'
+import { render } from 'react-dom'
+import './index.scss'
+
+const Application: FC = () => {
+    return<div>1</div>
+}
+
+render(<Application />, document.getElementById('application'))
