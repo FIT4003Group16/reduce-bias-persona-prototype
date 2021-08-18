ca3c487688efdc0bc6b0d2c5f81df9617262ec1b
Chun Tse <chuntsexj@gmail.com>
2021-08-18 16:34:15 +0800

    test git

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import './index.scss'
 
 const Application: FC = () => {
-    return<div>1</div>
+    return<div>persona</div>
 }
 
 render(<Application />, document.getElementById('application'))
